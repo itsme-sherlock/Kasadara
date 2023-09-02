df9a701d1b38c76168e850c63a14b29ff189a70e
itsme-sherlock <127415025+itsme-sherlock@users.noreply.github.com>
2023-09-01 23:48:25 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// src/App.js
+
+import React from 'react';
+import NavBar from './components/NavBar';
+import MainContent from './components/MainContent';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <MainContent/>
+      
+    </div>
+
+  );
+}
+
+export default App;
